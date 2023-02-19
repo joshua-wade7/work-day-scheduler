2cfbc19f8c55db509dc081bc0dd8be3d48bb0eff
joshua-wade7 <joshua.nelson0124@gmail.com>
2023-02-19 12:19:48 -0700

    began working on funcitions in script.js and added remaining time blocks within html file.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,16 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+//Variable Declarations at top
+var userInput = $(".description");
+
+var displayDate = $("#currentDay");
+
+function currentDate() {
+  displayDate.text(dayjs().format("dddd MMM, DD"));
+}
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +31,4 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+currentDate();
